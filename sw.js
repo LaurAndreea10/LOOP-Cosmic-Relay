15a7d6e64717f48928fce3ef27b83be4c40eebb0
LaurAndreea10 <79794410+LaurAndreea10@users.noreply.github.com>
2026-09-13 15:28:38 +0300

    Fix offline cache for GitHub Pages

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='loop-premium-v3';
-const ASSETS=['./','./LOOP-Cosmic-Relay.html','./manifest.webmanifest','./loop-icon.svg'];
+const CACHE='loop-premium-v4';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./loop-icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{let copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./LOOP-Cosmic-Relay.html'))))});
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{let copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html'))))});
