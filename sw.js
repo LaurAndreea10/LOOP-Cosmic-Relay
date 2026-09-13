61d69afc0868103e9d7d7cb8b8781457ce5669d9
LaurAndreea10 <79794410+LaurAndreea10@users.noreply.github.com>
2026-09-13 16:04:09 +0300

    Refresh PWA cache for responsive layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='loop-premium-v4';
+const CACHE='loop-premium-v5';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./loop-icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
